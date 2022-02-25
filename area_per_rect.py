591e84878951645c8478a2055c6d202b781dec86
Ubuntu <ubuntu@ip-172-31-45-190.ec2.internal>
2022-02-25 00:39:43 +0000

    Commiting area and perimeter of a rectangle as well as basic_math.py

---
+++ area_per_rect.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+# Created by Marc Coffi
+# Created on February 2022
+# This is a program that calculates the area and perimeter of a rectangle
+
+
+def main():
+    print("If a rectangle has the dimensions: 4cm x 6cm")
+    print("The Area is {}cm^2".format(4*6))
+    print("The Perimeter is {}cm".format(2*(4+6)))
+
+
+if __name__ == "__main__":
+    main()
---
+++ basic_math.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+# Created by Marc Coffi
+# Created on February 2022
+# This is a program that runs basic math operations
+
+
+def main():
+    print("4+2={}".format(4+2))
+    print("4-2={}".format(4-2))
+    print("4*2={}".format(4*2))
+    print("4/2={}".format(4/2))
+    print("4**2={}".format(4**2))
+    print("square root of 81={}".format(81**0.5))
+
+
+if __name__ == "__main__":
+    main()
